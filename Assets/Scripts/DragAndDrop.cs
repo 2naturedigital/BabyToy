43b15702d30d88084cbf4a4a2d25171de07034bd
James_Desktop <videovillain@gmail.com>
2020-10-29 18:02:43 +0900

    Movement and sound
    Added movement to each fish
    Added ability to collide and be tapped
    Added background for collisions to be handled but they are not handled yet
    Added sound playing

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    bool moveAllowed;
+    Collider2D col;
+
+    private RandomPatrol randomPatrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    void Awake()
+    {
+        randomPatrol = GameObject.FindObjectOfType<RandomPatrol>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            // When a touch begins, rab its location and see if it is overlaping a collider2d object then set that object to moveable
+            if (touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchedCollider)
+                {
+                    moveAllowed = true;
+                }
+            }
+
+            // Move the object to where the touch is moving
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (moveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            }
+
+            // Turn off movable when touch is ended
+            if (touch.phase == TouchPhase.Ended)
+            {
+                moveAllowed = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        RandomPatrol rp = other.gameObject.GetComponent<RandomPatrol>();
+        Vector2 target = randomPatrol.GetCurrentTarget();
+        randomPatrol.UpdateTarget(rp.GetCurrentTarget());
+        rp.UpdateTarget(target);
+    }
+}
---
+++ Assets/Scripts/RandomPatrol.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomPatrol : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float speed;
+
+    // Keep track of current target position
+    private Vector2 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get a starting target position
+        targetPosition = GetRandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do a movement transformation if the target position and the current position don't match
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        return new Vector2(randomX, randomY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Reload screen when there is a collision
+        if (other.tag == "Fish")
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    public void UpdateTarget(Vector2 newTarget)
+    {
+        targetPosition = newTarget;
+    }
+
+    public Vector2 GetCurrentTarget()
+    {
+        return targetPosition;
+    }
+}
