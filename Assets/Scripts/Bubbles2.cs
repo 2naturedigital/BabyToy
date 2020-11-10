0bc20665e6606c4c6bc9e777b61442679c425610
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-10 09:22:14 +0900

    added bubble clone script

--- Assets/Scripts/Bubbles2.cs
+++ Assets/Scripts/Bubbles2.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Bubbles2 : MonoBehaviour
 {
     public GameObject bubbleOriginal;
-    public GameObject bubblesContainer;
+    public GameObject bubbleContainer;
 
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject bubbleClone = Instantiate(bubbleOriginal);
+        bubbleClone.transform.parent = bubbleContainer.transform;
     }
 }
