7fe8c290b102d3e0abbffa18063f473315d07c9e
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-10 16:51:44 +0900

    modified bubble script

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    private float bubbleVelocity = 0;
+    public int bubbleSpeed = 1000;
+    private int direction = 0;
+    public int movementPeriod = 3;
+    private float elapsedTime = 0;
+
+
+
     void start() {
-        
+        direction = Random.Range(1, 2);
+        bubbleVelocity = bubbleSpeed * Time.deltaTime;
+        //set rando bubble sounds here
     }
     
-    // Update is called once per frame
+    
     void Update() {
-        
+        //set bubble movements here        
+
+        if (elapsedTime > movementPeriod) {
+            switch (direction) {
+                case 1:
+                GetComponent<Rigidbody2D>().velocity = Vector2.left * bubbleVelocity;
+                break;
+
+                case 2:
+                GetComponent<Rigidbody2D>().velocity = Vector2.right * bubbleVelocity;
+                break;
+            }            
+        } else {
+            elapsedTime += Time.deltaTime;
+        }        
     }
 }
