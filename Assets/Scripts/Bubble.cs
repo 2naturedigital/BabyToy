9609ffeeb35d407edc8ccffd2633a2f124b1b160
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-10 20:51:36 +0900

    updated bubbles again

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    public Animator animator;
+    Collider2D col;
     private float bubbleVelocity = 0;
     public int bubbleSpeed = 1000;
     private int direction = 0;
@@ -13,8 +15,8 @@ public class Bubble : MonoBehaviour
 
 
     void start() {
-        direction = Random.Range(1, 2);
         bubbleVelocity = bubbleSpeed * Time.deltaTime;
+        col = GetComponent<Collider2D>();
         //set rando bubble sounds here
     }
     
@@ -23,6 +25,7 @@ public class Bubble : MonoBehaviour
         //set bubble movements here        
 
         if (elapsedTime > movementPeriod) {
+            direction = Random.Range(0, 3);
             switch (direction) {
                 case 1:
                 GetComponent<Rigidbody2D>().velocity = Vector2.left * bubbleVelocity;
@@ -31,9 +34,26 @@ public class Bubble : MonoBehaviour
                 case 2:
                 GetComponent<Rigidbody2D>().velocity = Vector2.right * bubbleVelocity;
                 break;
-            }            
+            }
+            elapsedTime = 0;            
         } else {
             elapsedTime += Time.deltaTime;
-        }        
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            // When a touch begins, grab its location and see if it is overlaping a collider2d object
+            if (touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchedCollider)
+                {
+                    animator.SetTrigger("Touched");
+                }
+            }            
+        }
     }
 }
--- Assets/Scripts/Bubbles2.cs
+++ Assets/Scripts/Bubbles2.cs
@@ -6,11 +6,9 @@ public class Bubbles2 : MonoBehaviour
 {
     public GameObject bubbleOriginal;
     public GameObject bubbleContainer;
-    public Animator animator;
-    Collider2D col;
     readonly float bubbleSpawnMinTime = 1;
     readonly float bubbleSpawnMaxTime = 8;
-    readonly float bubblePopMinTime = 5;
+    readonly float bubblePopMinTime = 10;
     readonly float bubblePopMaxTime = 20;
     float bubbleTimer = 0;
     float bubbleDestoryTimer = 0;
@@ -19,14 +17,14 @@ public class Bubbles2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {        
-        col = GetComponent<Collider2D>();
+        
     }
 
     public void CreateBubbles() {          
         if (bubbleTimer <= 0) {
             Vector3 bubblePosition = new Vector3(Random.Range(-540.0f, 540.0f), -1000.0f, 0f);
             GameObject bubbleClone = Instantiate(bubbleOriginal, bubblePosition, bubbleOriginal.transform.rotation);
-            bubbleClone.transform.parent = bubbleContainer.transform;
+            bubbleClone.transform.SetParent(bubbleContainer.transform);
 
             //create a self-destruct timer for the bubble just created and set it
             bubbleDestoryTimer = Random.Range(bubblePopMinTime, bubblePopMaxTime);
@@ -45,21 +43,7 @@ public class Bubbles2 : MonoBehaviour
 
         CreateBubbles(); //need the number to be dynamic, reactive to the shake. 
 
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-            // When a touch begins, grab its location and see if it is overlaping a collider2d object
-            if (touch.phase == TouchPhase.Began)
-            {
-                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-                if (col == touchedCollider)
-                {
-                    animator.SetTrigger("Touched");
-                }
-            }            
-        }
+        
     }
 
     
