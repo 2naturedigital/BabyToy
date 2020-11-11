35efe5fd9fd75b3e55bab53e0e19e1034506ff2b
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-11 10:05:49 +0900

    added water current

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -6,54 +6,34 @@ public class Bubble : MonoBehaviour
 {
     public Animator animator;
     Collider2D col;
-    private float bubbleVelocity = 0;
-    public int bubbleSpeed = 1000;
-    private int direction = 0;
-    public int movementPeriod = 3;
-    private float elapsedTime = 0;
+    Rigidbody2D rb;
 
 
 
     void start() {
-        bubbleVelocity = bubbleSpeed * Time.deltaTime;
+        rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         //set rando bubble sounds here
-    }
+    }    
     
-    
-    void Update() {
-        //set bubble movements here        
+    void Update() {      
 
-        if (elapsedTime > movementPeriod) {
-            direction = Random.Range(0, 3);
-            switch (direction) {
-                case 1:
-                GetComponent<Rigidbody2D>().velocity = Vector2.left * bubbleVelocity;
-                break;
+        
 
-                case 2:
-                GetComponent<Rigidbody2D>().velocity = Vector2.right * bubbleVelocity;
-                break;
-            }
-            elapsedTime = 0;            
-        } else {
-            elapsedTime += Time.deltaTime;
-        }
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+    //        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
             // When a touch begins, grab its location and see if it is overlaping a collider2d object
-            if (touch.phase == TouchPhase.Began)
-            {
-                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-                if (col == touchedCollider)
-                {
-                    animator.SetTrigger("Touched");
-                }
-            }            
-        }
+    //        if (touch.phase == TouchPhase.Began)
+    //        {
+    //            Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+    //            if (col == touchedCollider)
+    //            {
+    //                animator.SetTrigger("Touched");
+    //            }
+    //        }            
+    //    }
     }
 }
---
+++ Assets/Scripts/waterCurrent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterCurrent : MonoBehaviour {
+   
+   List<Rigidbody2D> bubblesInCurrent = new List<Rigidbody2D>();
+   public float currentStrength;
+   public Vector2 currentDirection;
+
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        Rigidbody2D objectRigid = col.gameObject.GetComponent<Rigidbody2D>();
+        if (objectRigid != null)
+            bubblesInCurrent.Add(objectRigid);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D col) {
+        Rigidbody2D objectRigid = col.gameObject.GetComponent<Rigidbody2D>();
+        if (objectRigid != null)
+            bubblesInCurrent.Remove(objectRigid);
+    }
+
+
+    private void FixedUpdate() {        
+        if (bubblesInCurrent.Count > 0) {
+            foreach (Rigidbody2D bubble in bubblesInCurrent) {
+                if (bubble == null) {
+                    bubblesInCurrent.Remove(bubble);
+                } else {
+                    bubble.AddForce(currentStrength * currentDirection);
+                }
+                
+            }
+        }
+    }
+}
