22f36e5429b53c0477f874a386c87b1fff0ac73c
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-11 11:02:04 +0900

    bubbles updated

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -13,6 +13,7 @@ public class Bubble : MonoBehaviour
     void start() {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
         //set rando bubble sounds here
     }    
     
@@ -20,20 +21,16 @@ public class Bubble : MonoBehaviour
 
         
 
-    //    if (Input.touchCount > 0)
-    //    {
-    //        Touch touch = Input.GetTouch(0);
-    //        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
             // When a touch begins, grab its location and see if it is overlaping a collider2d object
-    //        if (touch.phase == TouchPhase.Began)
-    //        {
-    //            Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-    //            if (col == touchedCollider)
-    //            {
-    //                animator.SetTrigger("Touched");
-    //            }
-    //        }            
-    //    }
+            if (touch.phase == TouchPhase.Began) {
+                if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition)) {
+                    animator.SetTrigger("Touched");
+                }
+            }            
+        }
     }
 }
