f2e5b63ec3ddde4dea840c2d4a44eea67f3b6bd8
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-12 11:04:18 +0900

    bubble pop sounds added, bubbles destroyed on touch

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//TODO -- add a method to destroy bubble after it is touched (and popped)
-//TODO -- add sounds for pop (randomized)
 //TODO -- randomize sounds for spawned bubbles
 
 public class Bubble : MonoBehaviour
 {
     public Animator animator;
+    public AudioSource audioSrc;
+    public AudioClip[] audioClipArray;
+    public float destroyTimer;
+    private GameObject bubble;
     Collider2D col;
     Rigidbody2D rb;
-
 
 
     void start() {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+        audioSrc = GetComponent<AudioSource>();
         //set rando bubble sounds here
     }    
     
     void Update() {      
-
         
 
         if (Input.touchCount > 0) {
@@ -33,9 +33,17 @@ public class Bubble : MonoBehaviour
             // When a touch begins, grab its location and see if it is overlaping a collider2d object
             if (touch.phase == TouchPhase.Began) {
                 if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition)) {
-                    animator.SetTrigger("Touched");
+                    bubble = GetComponent<Collider2D>().gameObject;
+                    Destroyer();
                 }
             }            
         }
+    }
+
+    private void Destroyer() {
+        animator.SetTrigger("Touched");
+        audioSrc.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        audioSrc.PlayOneShot(audioSrc.clip);
+        Destroy(bubble, destroyTimer);
     }
 }
---
+++ Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds : MonoBehaviour
+{
+
+    public AudioSource audioSrc;
+    public AudioClip[] audioClipArray;
+
+
+    private void Awake() {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+
+    // Start is called before the first frame update
+    public void Start() {
+        audioSrc.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        audioSrc.PlayOneShot(audioSrc.clip);
+    }
+
+   
+}
