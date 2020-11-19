62fcf97b938251dc8d9036bc20c534f1fa53cee5
LemonPlagueUnity <moorerichardplease@gmail.com>
2020-11-19 17:36:30 +0900

    wut

--- Assets/Scripts/BlowFish.cs
+++ Assets/Scripts/BlowFish.cs
@@ -5,9 +5,14 @@ public class BlowFish : FishController
     public float pumpMinTime = 0.3f;
     public float pumpMaxTime = 3f;
     float pumpTimer = 0;
+    public float pumpPower;
+    private Vector2 pumpDirection;
+    Rigidbody2D blowFish;
 
     // Start is called before the first frame update
     void Start() {
+        blowFish = GetComponent<Rigidbody2D>();
+        pumpDirection = Vector2.up;
     }
 
     void Awake() {
@@ -16,15 +21,15 @@ public class BlowFish : FishController
     // Update is called once per frame
     void Update() {
         AnimateFish();
-        MoveFish();
     }
 
+    //blowfish pump animation
     public override void AnimateFish()
     {
         base.AnimateFish();
-        //blowfish pump
         if (pumpTimer <= 0) {
             animator.SetTrigger("pumpOnce");
+            MoveFish();            
             pumpTimer = Random.Range(pumpMinTime, pumpMaxTime);
         } else {
             pumpTimer -= Time.deltaTime;
@@ -33,8 +38,10 @@ public class BlowFish : FishController
 
     public override void MoveFish()
     {
-        //base.MoveFish();
+        base.MoveFish();
+        blowFish.velocity = pumpDirection * pumpPower;
     }
+
     private void OnTriggerEnter2D(Collider2D other) {
 
     }
