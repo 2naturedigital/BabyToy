eae11d7906fb75c91eb6de375d766e20d6d1640d
James_Desktop <videovillain@gmail.com>
2020-11-26 16:56:22 +0900

    Added multiple bubbles per shake!
    You can change the rate they are produced and how many are produced each time with shake bubble timer and counter

--- Assets/Scripts/BubblesDup.cs
+++ Assets/Scripts/BubblesDup.cs
@@ -12,7 +12,10 @@ public class BubblesDup : MonoBehaviour
     public float bubblePopMaxTime;
     public float bubbleMinScale;
     public float bubbleMaxScale;
+    private bool isShaking = false;
     float bubbleTimer = 0;
+    public float shakeBubbleTimer;
+    public int shakeBubbleCount;
 
     // Update is called once per frame
     void Update() {
@@ -20,19 +23,36 @@ public class BubblesDup : MonoBehaviour
         CreateBubbles(); //need the number to be dynamic, reactive to the shake.
     }
 
+    public void StartShake(Vector3 mult) {
+        isShaking = true;
+        bubbleTimer = shakeBubbleTimer;
+    }
+
+    public void EndShake() {
+        isShaking = false;
+    }
 
     public void CreateBubbles() {
-        if (bubbleTimer <= 0) {
+        if (isShaking) {
+            if (bubbleTimer <= 0) {
+                MakeBubble(shakeBubbleCount);
+                bubbleTimer = shakeBubbleTimer;
+            }
+        } else if (bubbleTimer <= 0) {
+            MakeBubble(1);
+            //create a self-destruct timer for the bubble just created and set it
+            //bubbleDestoryTimer = Random.Range(bubblePopMinTime, bubblePopMaxTime);
+            //Destroy(bubbleClone, bubbleDestoryTimer);
+            bubbleTimer = Random.Range(bubbleSpawnMinTime, bubbleSpawnMaxTime);
+        }
+    }
+
+    public void MakeBubble(int count) {
+        for (int i = 0; i < count; i++) {
             Vector3 bubblePosition = new Vector3(Random.Range(-540.0f, 540.0f), -1000.0f, 0f);
             GameObject bubbleClone = Instantiate(bubbleOriginal, bubblePosition, bubbleOriginal.transform.rotation);
             bubbleClone.transform.SetParent(bubbleContainer.transform);
             bubbleClone.transform.localScale *= Random.Range(bubbleMinScale, bubbleMaxScale);
-
-            //create a self-destruct timer for the bubble just created and set it
-            //bubbleDestoryTimer = Random.Range(bubblePopMinTime, bubblePopMaxTime);
-            //Destroy(bubbleClone, bubbleDestoryTimer);
-
-            bubbleTimer = Random.Range(bubbleSpawnMinTime, bubbleSpawnMaxTime);
         }
     }
 }
