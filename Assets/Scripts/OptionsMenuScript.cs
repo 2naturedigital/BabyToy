2a06890ce21544541263f81055763e369d9b679c
James_Desktop <videovillain@gmail.com>
2020-12-14 21:58:23 +0900

    Added a reset to the options
    The reset needs to be changed manually for each additional option added (for now at least)

--- Assets/Scripts/OptionsMenuScript.cs
+++ Assets/Scripts/OptionsMenuScript.cs
@@ -1,45 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-//using UnityEngine.UI; // Required when Using UI elements.
+//using UnityEngine.UI;
 
 public class OptionsMenuScript : MonoBehaviour
 {
-    //public Slider mainSlider;
-    private float currentVolume;
-    private int currentShakePower;
-    private float currentBubbleAmount;
-    private int currentBubbleCount;
+    public float currentVolume = 1f;
+    public float currentShakePower = 1f;
+    public float currentBubbleAmount = 0.15f;
+    public float currentBubbleCount = 1f;
+
+    void Awake() {
+        Debug.Log("Volume: " + currentVolume);
+        Debug.Log("Power: " + currentShakePower);
+        Debug.Log("Bubble Amount: " + currentBubbleAmount);
+        Debug.Log("Bubble Count: " + currentBubbleCount);
+    }
 
     public void VolumeChanged(float vol) {
         // Store volume changes for use by rattler
-        //Debug.Log("Volume Changed");
         currentVolume = vol;
-        //Debug.Log("Volume: " + currentVolume);
+        Debug.Log("Volume: " + currentVolume);
     }
 
-    public void ShakePowerChanged(int power) {
+    public void ShakePowerChanged(float power) {
         // Store shake power changes for use by rattler
         currentShakePower = power;
+        Debug.Log("Power: " + currentShakePower);
     }
 
     public void BubbleAmountChanged(float amount) {
         // Store bubble amount changes for use by rattler
         currentBubbleAmount = amount;
-        //Debug.Log("Bubble Amount: " + currentBubbleAmount);
+        Debug.Log("Bubble Amount: " + currentBubbleAmount);
     }
 
-    public void BubbleCountChanged(int count) {
+    public void BubbleCountChanged(float count) {
         // Store shake power changes for use by rattler
         currentBubbleCount = count;
+        Debug.Log("Bubble Count: " + currentBubbleCount);
     }
 
 
     public void DefaultSettings() {
         // Reset defaults
-        currentVolume = 1f;
-        currentShakePower = 1;
-        currentBubbleAmount = .15f;
-        currentBubbleCount = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 
