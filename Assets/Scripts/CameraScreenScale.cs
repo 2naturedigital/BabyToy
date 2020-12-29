928d55e61040aa76e51887c8a25a24b1490fbdf9
James_Desktop <videovillain@gmail.com>
2020-12-29 12:30:03 +0900

    only grabs camera once instead of every time

--- Assets/Scripts/CameraScreenScale.cs
+++ Assets/Scripts/CameraScreenScale.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class CameraScreenScale : MonoBehaviour {
     public SpriteRenderer bg;
     public Button button;
+    public CanvasRenderer canvasRenderer;
     public float holdTimer = 5.0f;
     private float elapsedTime = 0;
     public SpriteRenderer waterLayer;
@@ -17,12 +18,17 @@ public class CameraScreenScale : MonoBehaviour {
     private int screenHeight;
     private int screenWidth;
     private bool landscape;
+    private Camera cameraMain;
     // public bool maintainWidth = false;
     // [Range(-1,1)]
     // public int adaptPosition;
     // private Vector3 CameraPos;
     // private float defaultWidth;
     // private float defaultHeight;
+
+    private void Awake() {
+        cameraMain = Camera.main;
+    }
 
     void Start() {
         // Calculate screen ratio
@@ -32,9 +38,9 @@ public class CameraScreenScale : MonoBehaviour {
 
 
         /*In case we decide to go with landscape rotation **
-        CameraPos = Camera.main.transform.position;
-        defaultHeight = Camera.main.orthographicSize;
-        defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        CameraPos = cameraMain.transform.position;
+        defaultHeight = cameraMain.orthographicSize;
+        defaultWidth = cameraMain.orthographicSize * cameraMain.aspect;
         */
     }
 
@@ -82,23 +88,26 @@ public class CameraScreenScale : MonoBehaviour {
 
         // In case we decide to go with landscape rotation **
         // if (maintainWidth) {
-        //     Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
-        //     Camera.main.transform.position = new Vector3(CameraPos.x, adaptPosition * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+        //     cameraMain.orthographicSize = defaultWidth / cameraMain.aspect;
+        //     cameraMain.transform.position = new Vector3(CameraPos.x, adaptPosition * (defaultHeight - cameraMain.orthographicSize), CameraPos.z);
         // } else {
-        //     Camera.main.transform.position = new Vector3(adaptPosition * (defaultWidth - Camera.main.orthographicSize * Camera.main.aspect), CameraPos.y, CameraPos.z);
+        //     cameraMain.transform.position = new Vector3(adaptPosition * (defaultWidth - cameraMain.orthographicSize * cameraMain.aspect), CameraPos.y, CameraPos.z);
         // }
     }
 
     public void OnClick() {
-        while (Input.touches[0].phase == TouchPhase.Stationary) {
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= holdTimer) {
-                SceneManager.LoadScene("Menu");
-            }
+        if (Input.GetMouseButtonUp(0)) {
+            SceneManager.LoadScene("Menu");
         }
-        if (Input.touches[0].phase == TouchPhase.Ended) {
-            elapsedTime = 0;
-        }
+        // while (Input.touches[0].phase == TouchPhase.Stationary) {
+        //     elapsedTime += Time.deltaTime;
+        //     if (elapsedTime >= holdTimer) {
+        //         SceneManager.LoadScene("Menu");
+        //     }
+        // }
+        // if (Input.touches[0].phase == TouchPhase.Ended) {
+        //     elapsedTime = 0;
+        // }
     }
 
     // Possibly a much simpler way to calculate screen ortho and sprite ratios
@@ -113,25 +122,29 @@ public class CameraScreenScale : MonoBehaviour {
         // If the screen's height is greater than the bacground's height, stretch ortho to fit BG top to bottom
         // Otherwise, stretch the ortho to fit BG side to side and adjust hand size if needed
         if (Screen.height > bg.bounds.size.y) {
-            Camera.main.orthographicSize = bg.bounds.size.y/2;
+            cameraMain.orthographicSize = bg.bounds.size.y/2;
             leftHand.transform.localScale *= 0.8f;
             rightHand.transform.localScale *= 0.8f;
         } else {
-            Camera.main.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
+            cameraMain.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
         }
 
         // Move the hands to the sides of the screen
-        float newLeftX = -(Camera.main.orthographicSize * Camera.main.aspect) + leftHand.bounds.size.x/2;
-        float newLeftY = -(Camera.main.orthographicSize) + leftHand.bounds.size.y/2;
-        float newRightX = (Camera.main.orthographicSize * Camera.main.aspect) - rightHand.bounds.size.x/2;
-        float newRightY = -(Camera.main.orthographicSize) + rightHand.bounds.size.y/2;
+        float newLeftX = -(cameraMain.orthographicSize * cameraMain.aspect) + leftHand.bounds.size.x/2;
+        float newLeftY = -(cameraMain.orthographicSize) + leftHand.bounds.size.y/2;
+        float newRightX = (cameraMain.orthographicSize * cameraMain.aspect) - rightHand.bounds.size.x/2;
+        float newRightY = -(cameraMain.orthographicSize) + rightHand.bounds.size.y/2;
         leftHand.transform.position = new Vector3(newLeftX,newLeftY,0);
         rightHand.transform.position = new Vector3(newRightX,newRightY,0);
 
         // Move Parent Button
-        float newX = (Camera.main.orthographicSize * Camera.main.aspect) - button.image.minWidth/2;
-        float newY = (Camera.main.orthographicSize) - button.image.minHeight/2;
+        float newX = -(cameraMain.orthographicSize * cameraMain.aspect) - canvasRenderer.transform.position.x/2;
+        float newY = (cameraMain.orthographicSize) - canvasRenderer.transform.position.y/2;
+        Debug.Log("btn x: " + newX + " btn y: " + newY);
         button.transform.position = new Vector3(newX,newY,0);
+        canvasRenderer.transform.position = new Vector3(newX,newY,0);
+        Debug.Log("Button: " + button.transform.position);
+        Debug.Log("CanvasRenderer: " + canvasRenderer.transform.position);
     }
 
     // Older way to calculate screen ortho and sprite ratios
@@ -160,32 +173,32 @@ public class CameraScreenScale : MonoBehaviour {
     //     if (landscape) {
     //         //Debug.Log("CameraScreenScale - Landscape");
     //         if (backgroundRatio > screenRatio) {
-    //             Camera.main.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
+    //             cameraMain.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
     //             //Debug.Log("CameraScreenScale - BackgroundRatio Larger");
     //         } else {
-    //             Camera.main.orthographicSize = bg.bounds.size.y/2;
+    //             cameraMain.orthographicSize = bg.bounds.size.y/2;
     //             //Debug.Log("CameraScreenScale - ScreenRatio Larger");
     //         }
     //     } else {
     //         //Debug.Log("CameraScreenScale - Portrait");
     //         if (backgroundRatio > screenRatio) {
-    //             Camera.main.orthographicSize = bg.bounds.size.y/2;
+    //             cameraMain.orthographicSize = bg.bounds.size.y/2;
     //             leftHand.transform.localScale *= 0.8f;
     //             rightHand.transform.localScale *= 0.8f;
     //             //Debug.Log("CameraScreenScale - BackgroundRatio Larger");
     //         } else {
     //             if (Screen.height > bg.bounds.size.y) {
-    //                 Camera.main.orthographicSize = bg.bounds.size.y/2;
+    //                 cameraMain.orthographicSize = bg.bounds.size.y/2;
     //                 leftHand.transform.localScale *= 0.8f;
     //                 rightHand.transform.localScale *= 0.8f;
     //                 //Debug.Log("CameraScreenScale - ScreenRatio Larger - Screen taller than bg");
     //             } else {
-    //                 Camera.main.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
+    //                 cameraMain.orthographicSize = bg.bounds.size.x * Screen.height / Screen.width * 0.5f;
     //                 //Debug.Log("CameraScreenScale - ScreenRatio Larger - screen shorter than bg");
     //             }
     //         }
     //     }
-    //     //Debug.Log("CameraScreenScale - Ortho Size Set: " + Camera.main.orthographicSize);
+    //     //Debug.Log("CameraScreenScale - Ortho Size Set: " + cameraMain.orthographicSize);
     // }
 
     public float GetSpriteAdjustmentRatio() {
